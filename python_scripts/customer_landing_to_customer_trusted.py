212c3ac2b6a9387cd1f1ffd169cbfee4fa61fec2
jtw023 <R3stock3r@tutanota.com>
2024-06-04 19:00:06 +0200

    modified to remove PII and add data catalogue

--- python_scripts/customer_landing_to_customer_trusted.py
+++ python_scripts/customer_landing_to_customer_trusted.py
@@ -4,8 +4,14 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue import DynamicFrame
 import re
 
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -19,7 +25,27 @@ CustomerLanding_node1717487795825 = glueContext.create_dynamic_frame.from_option
 # Script generated for node Drop Non-Consenting
 DropNonConsenting_node1717487921527 = Filter.apply(frame=CustomerLanding_node1717487795825, f=lambda row: (not(row["shareWithResearchAsOfDate"] == 0)), transformation_ctx="DropNonConsenting_node1717487921527")
 
-# Script generated for node Amazon S3
-AmazonS3_node1717488080223 = glueContext.write_dynamic_frame.from_options(frame=DropNonConsenting_node1717487921527, connection_type="s3", format="json", connection_options={"path": "s3://jwawss3testbucket/customers/trusted/", "partitionKeys": []}, transformation_ctx="AmazonS3_node1717488080223")
+# Script generated for node Remove PII
+SqlQuery1764 = '''
+SELECT
+    serialNumber,
+    birthDay,
+    registrationDate,
+    shareWithResearchAsOfDate,
+    CONCAT(SUBSTRING(SPLIT_PART(customerName, ' ', 1), 0, 1), SUBSTRING(SPLIT_PART(customerName, ' ', 2), 0, 1)) AS customerInitials,
+    shareWithFriendsAsOfDate,
+    SPLIT_PART(email, '@', 2) AS emailDomain,
+    lastUpdateDate,
+    phone,
+    shareWithPublicAsOfDate
+FROM myDataSource
 
+'''
+RemovePII_node1717515620498 = sparkSqlQuery(glueContext, query = SqlQuery1764, mapping = {"myDataSource":DropNonConsenting_node1717487921527}, transformation_ctx = "RemovePII_node1717515620498")
+
+# Script generated for node Amazon S3
+AmazonS3_node1717488080223 = glueContext.getSink(path="s3://jwawss3testbucket/customers/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1717488080223")
+AmazonS3_node1717488080223.setCatalogInfo(catalogDatabase="customer trusted",catalogTableName="customer_catalog")
+AmazonS3_node1717488080223.setFormat("json")
+AmazonS3_node1717488080223.writeFrame(RemovePII_node1717515620498)
 job.commit()
--- python_scripts/customer_trusted_to_customer_curated.py
+++ python_scripts/customer_trusted_to_customer_curated.py
@@ -5,8 +5,14 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from awsglue.dynamicframe import DynamicFrame
+from awsglue import DynamicFrame
 from pyspark.sql import functions as SqlFuncs
 
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -17,19 +23,46 @@ job.init(args['JOB_NAME'], args)
 # Script generated for node Accelerometer Trusted
 AccelerometerTrusted_node1717491250474 = glueContext.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="json", connection_options={"paths": ["s3://jwawss3testbucket/accelerometer/trusted/"], "recurse": True}, transformation_ctx="AccelerometerTrusted_node1717491250474")
 
-# Script generated for node Customer Trusted
-CustomerTrusted_node1717491078297 = glueContext.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="json", connection_options={"paths": ["s3://jwawss3testbucket/customers/trusted/"], "recurse": True}, transformation_ctx="CustomerTrusted_node1717491078297")
+# Script generated for node Customer Landing
+CustomerLanding_node1717517901802 = glueContext.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="json", connection_options={"paths": ["s3://jwawss3testbucket/customers/landing/"], "recurse": True}, transformation_ctx="CustomerLanding_node1717517901802")
+
+# Script generated for node Remove Non-Consensual
+SqlQuery1684 = '''
+SELECT
+    *
+FROM myDataSource
+WHERE shareWithResearchAsOfDate IS NOT NULL
+
+'''
+RemoveNonConsensual_node1717519529420 = sparkSqlQuery(glueContext, query = SqlQuery1684, mapping = {"myDataSource":CustomerLanding_node1717517901802}, transformation_ctx = "RemoveNonConsensual_node1717519529420")
 
 # Script generated for node Only Accelerometer Join
-OnlyAccelerometerJoin_node1717491210216 = Join.apply(frame1=CustomerTrusted_node1717491078297, frame2=AccelerometerTrusted_node1717491250474, keys1=["email"], keys2=["user"], transformation_ctx="OnlyAccelerometerJoin_node1717491210216")
+OnlyAccelerometerJoin_node1717491210216 = Join.apply(frame1=AccelerometerTrusted_node1717491250474, frame2=RemoveNonConsensual_node1717519529420, keys1=["user"], keys2=["email"], transformation_ctx="OnlyAccelerometerJoin_node1717491210216")
 
-# Script generated for node Drop Fields
-DropFields_node1717491713445 = DropFields.apply(frame=OnlyAccelerometerJoin_node1717491210216, paths=["z", "user", "y", "x", "timestamp"], transformation_ctx="DropFields_node1717491713445")
+# Script generated for node Remove PII
+SqlQuery1685 = '''
+SELECT
+    serialNumber,
+    birthDay,
+    registrationDate,
+    shareWithResearchAsOfDate,
+    CONCAT(SUBSTRING(SPLIT_PART(customerName, ' ', 1), 0, 1), SUBSTRING(SPLIT_PART(customerName, ' ', 2), 0, 1)) AS customerInitials,
+    shareWithFriendsAsOfDate,
+    SPLIT_PART(email, '@', 2) AS emailDomain,
+    lastUpdateDate,
+    phone,
+    shareWithPublicAsOfDate
+FROM myDataSource
+
+'''
+RemovePII_node1717518131144 = sparkSqlQuery(glueContext, query = SqlQuery1685, mapping = {"myDataSource":OnlyAccelerometerJoin_node1717491210216}, transformation_ctx = "RemovePII_node1717518131144")
 
 # Script generated for node Drop Duplicates
-DropDuplicates_node1717491662868 =  DynamicFrame.fromDF(DropFields_node1717491713445.toDF().dropDuplicates(), glueContext, "DropDuplicates_node1717491662868")
+DropDuplicates_node1717491662868 =  DynamicFrame.fromDF(RemovePII_node1717518131144.toDF().dropDuplicates(), glueContext, "DropDuplicates_node1717491662868")
 
 # Script generated for node Customers Curated
-CustomersCurated_node1717491875178 = glueContext.write_dynamic_frame.from_options(frame=DropDuplicates_node1717491662868, connection_type="s3", format="json", connection_options={"path": "s3://jwawss3testbucket/customers/curated/", "compression": "snappy", "partitionKeys": []}, transformation_ctx="CustomersCurated_node1717491875178")
-
+CustomersCurated_node1717491875178 = glueContext.getSink(path="s3://jwawss3testbucket/customers/curated/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], compression="snappy", enableUpdateCatalog=True, transformation_ctx="CustomersCurated_node1717491875178")
+CustomersCurated_node1717491875178.setCatalogInfo(catalogDatabase="customer curated",catalogTableName="customer_catalog")
+CustomersCurated_node1717491875178.setFormat("json")
+CustomersCurated_node1717491875178.writeFrame(DropDuplicates_node1717491662868)
 job.commit()
